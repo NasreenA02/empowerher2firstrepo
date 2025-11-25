c812ddc718cc5767a0a70abc8cc963122d51569d
NasreenA02 <nassu324@gmail.com>
2025-11-25 09:29:15 +0530

    callbacks and higher function

---
+++ Assignment10/que13.js
@@ -0,0 +1,22 @@
+
+function countCategories(categories) {
+
+  const counts = categories.reduce((acc, category) => {
+    acc[category] = (acc[category] || 0) + 1;
+    return acc;
+  }, {});
+  const sortedCategories = Object.entries(counts)
+    .sort((a, b) => b[1] - a[1])  
+    .map(entry => entry[0]);       
+
+  return { counts, sortedCategories };
+}
+
+
+const categories = ["electronics", "clothing", "electronics", "toys", "clothing", "toys", "toys"];
+
+
+const result = countCategories(categories);
+
+console.log(result.counts);           
+
